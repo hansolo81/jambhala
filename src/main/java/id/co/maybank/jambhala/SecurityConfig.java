bb349848c50cc73ab8b3ad8c46e9725e8580fb3c
00092666 <00092666>
2022-07-30 23:08:21 +0700

    feat: added swagger

--- src/main/java/id/co/maybank/jambhala/SecurityConfig.java
+++ src/main/java/id/co/maybank/jambhala/SecurityConfig.java
@@ -3,6 +3,7 @@ package id.co.maybank.jambhala;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs/**",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**");
     }
 
 }
--- src/main/java/id/co/maybank/jambhala/controller/TransferController.java
+++ src/main/java/id/co/maybank/jambhala/controller/TransferController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class TransferController {
     }
 
     @PostMapping("/intrabank")
-    public ResponseEntity<TransferResult> doIntrabankTransfer(@AuthenticationPrincipal Jwt jwt, TransferRequest request) {
+    public ResponseEntity<TransferResult> doIntrabankTransfer(@AuthenticationPrincipal Jwt jwt, @RequestBody TransferRequest request) {
         TransferResult transferResult = transferService.doIntrabank(jwt.getClaimAsString("pan"), request);
         return new ResponseEntity<>(transferResult, HttpStatus.OK);
     }
