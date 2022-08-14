2bc30285fd9314e3e6f3a28a6303c447d6784388
00092666 <00092666>
2022-08-14 20:24:49 +0700

    cleanup

--- src/test/java/id/co/maybank/jambhala/config/CucumberSpringConfiguration.java
+++ src/test/java/id/co/maybank/jambhala/config/CucumberSpringConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
-//@SpringBootTest(classes =JambhalaApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SpringBootTest
 class CucumberSpringConfiguration {
 }
